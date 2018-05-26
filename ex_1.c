d695645190d8fd435cfb3d51301873baf301f568
arnmatu <arnas.matusevicius@gmail.com>
2018-05-26 13:13:05 -0500

    ex_1 r0

--- ex_1.c
+++ ex_1.c
@@ -5,6 +5,7 @@ int main (int argc, char *argv[]){
   int distance = 100;
   //this is also comment
   printf("You are %d miles away.\n",distance );
+  //returns 0 single line comment
   return 0;
 
 }
