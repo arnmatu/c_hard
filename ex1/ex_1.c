80c823e7be3ebb00056cef726c49114441081cf4
arnmatu <arnas.matusevicius@gmail.com>
2018-05-26 13:18:51 -0500

    folder c

---
+++ ex1/ex_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*this is a comment.*/
+int main (int argc, char *argv[]){
+  int distance = 100;
+  //this is also comment
+  printf("You are %d miles away.\n",distance );
+  //returns 0 single line comment
+  return 0;
+
+}
