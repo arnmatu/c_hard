7e6d08b84addebce999aa9262abcc55759186367
arnmatu <arnas.matusevicius@gmail.com>
2018-05-27 16:11:44 -0500

    ex8

---
+++ ex4_debugger/ex4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main (int argc, char *argv[]){
+  int age=30;
+  int height=201;
+
+  printf("I am %d years old. \n",age);
+  printf("I am %d cm tall \n",height);
+
+  return 0;
+
+}
