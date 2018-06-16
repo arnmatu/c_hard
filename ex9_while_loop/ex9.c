5f7f70714f9e2a86bef81202eec28e034638625c
arnmatu <arnas.matusevicius@gmail.com>
2018-06-16 16:47:53 -0500

    11

---
+++ ex9_while_loop/ex9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  int i =0;
+  while (i<25){
+    printf("%d",i);
+    i++;
+  }
+  //need to add this to add final new line
+  printf("\n");
+}
